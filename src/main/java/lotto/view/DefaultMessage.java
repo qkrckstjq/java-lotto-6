944d9f14be42f6c8634e3c7c2ca3a3b9660a3911
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-11-29 17:59:49 +0900

    feat Winnings 당첨금 enum화

--- src/main/java/lotto/view/DefaultMessage.java
+++ src/main/java/lotto/view/DefaultMessage.java
@@ -5,6 +5,10 @@ public enum DefaultMessage {
     ASK_LOTTO_NUMBERS("당첨 번호를 입력해 주세요."),
     ASK_LOTTO_BONUS("보너스 번호를 입력해 주세요."),
     PURCHAS_NUMBER_MESSAGE("개를 구매했습니다."),
+    NEWLINE_MESSAGE("---"),
+    DEFAULT_RESULT("일치"),
+    BONUS_RESULT("일치, 보너스 볼 일치"),
+    NUMBER_MESSAGE("개"),
     LOTTO_RESULT_MESSAGE("당첨 통계");
 
     private String message;
