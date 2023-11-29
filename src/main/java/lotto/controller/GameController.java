04d810a0f3edfc92ef87a4e013a9648c88f05459
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-11-29 20:46:11 +0900

    feat AnswerValue 일치하는 로또 번호 enum 화

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,4 +1,22 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+import lotto.model.Validation;
+import lotto.view.InputView;
+
 public class GameController {
+    private Lotto answer;
+    private Lotto[] lottos;
+    public void run () {
+        buyPrice();
+    }
+
+    private int buyPrice () {
+        while(true) {
+            return Validation.isInteger(InputView.InputBuyPrice());
+        }
+    }
+
+
+
 }
