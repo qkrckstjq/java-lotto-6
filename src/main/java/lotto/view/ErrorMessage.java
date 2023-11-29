36a358980683f841fa93ca3de017bb6d2a84e8a3
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-11-29 18:13:59 +0900

    feat OutputView 작성

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
     MIN_MONEY("[ERROR] 1000원 이상 금액을 입력해주세요."),
     FORMAT_MONEY("[ERROR] 1000원 단위 금액을 입력해주세요."),
     INTEGER("[ERROR] 숫자만 입력 가능합니다."),
+    INVALID_NUMBER("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     FORMAT_NUMBERS("[ERROR] 올바른 형태의 로또 번호를 입력해주세요"),
     NUMBERS_LENGTH("[ERROR] 로또 번호는 6개를 입력해주세요.");
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,25 @@
 package lotto.view;
 
+import java.util.Arrays;
+
 public class OutputView {
 
+    public static void printArrayToString (int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void printResultMessage () {
+        System.out.println(DefaultMessage.LOTTO_RESULT_MESSAGE.getMessage());
+    }
+    public static void printNewLine () {
+        System.out.println(DefaultMessage.NEWLINE_MESSAGE.getMessage());
+    }
+    public static void printNormalResult (int winningNumber, String price, int sameResult) {
+        System.out.printf("%d개 일치 (%s) - %d개",winningNumber, price, sameResult);
+    }
+    public static void printBonusResult (int winningNumber, String price, int sameResult) {
+        System.out.printf("%d개 일치, 보너스 볼 일치 (%s) - %d개",winningNumber, price, sameResult);
+    }
+    public static void printWinningRate (float rate) {
+        System.out.printf("총 수익률은 %f%%입니다.",rate);
+    }
 }
