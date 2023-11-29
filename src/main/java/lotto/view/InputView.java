26b3adc0c0d6f400ed5e073bac07e5e1ec89be06
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-11-29 18:16:37 +0900

    feat InputView 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.sql.SQLOutput;
+
 public class InputView {
+    public static String InputBuyPrice() {
+        System.out.println(DefaultMessage.ASK_PRICE_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+
+    public static String InputLottoNumbers () {
+        System.out.println(DefaultMessage.ASK_LOTTO_NUMBERS.getMessage());
+        return Console.readLine();
+    }
+
+    public static String InputBonusNumber () {
+        System.out.println(DefaultMessage.ASK_LOTTO_BONUS.getMessage());
+        return Console.readLine();
+    }
 }
